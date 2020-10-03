1bee7c5d8c2d241d2185df5a7e10d7503e2a9c79
Atul Raj Gupta <raazatul7@gmail.com>
2020-10-03 17:47:22 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { View } from 'react-native'
 import { createAppContainer } from 'react-navigation';
 import { createRootNavigator } from './appRoot/Router';
 import LocalStorage from './common/LocalStorage';
-import PrintLog from './common/PrintLog';
+import PrintLog from './common/PrintLog';//Custom Console.log
 import firebase from 'react-native-firebase';
 
 export default class App extends Component {
@@ -15,28 +15,22 @@ export default class App extends Component {
     }
 
     async componentDidMount() {
-        //this.init();
-        // let isLogin = await LocalStorage.read("isLogin");
-        // PrintLog("isLogin >> ", isLogin);
-        // let isLogin = false;
-        // this.setState({ isLogin });
         firebase.auth().onAuthStateChanged(user => {
             this.setState({ isLogin: user });
-           // PrintLog('user', user)
         })
     }
 
     init = () => {
         if (!firebase.app.length) {
             firebase.initializeApp({
-                apiKey: "AIzaSyD_00558pMo6P9h9VDsbiVotNCUT2Pv9tc",
-                authDomain: "chat-e9b23.firebaseapp.com",
-                databaseURL: "https://chat-e9b23.firebaseio.com",
-                projectId: "chat-e9b23",
-                storageBucket: "chat-e9b23.appspot.com",
-                messagingSenderId: "85534229401",
-                appId: "1:85534229401:web:6fac334e3359d774a0c908",
-                measurementId: "G-X1CBK48L15"
+                apiKey: "Your_Key",
+                authDomain: "xxx",
+                databaseURL: "xxx",
+                projectId: "xxx-xxx",
+                storageBucket: "xxx",
+                messagingSenderId: "xxx",
+                appId: "xxx",
+                measurementId: "xxx"
             });
         }
     }
